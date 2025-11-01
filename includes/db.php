1ffc1b14d6e79b9cdada422abb73d91d9829dbeb
henrynitesh789 <niteshkumar802183@gmail.com>
2025-11-01 22:57:54 +0530

    DataBase

--- includes/db.php
+++ includes/db.php
@@ -1,7 +1,7 @@
 <?php
     $host = "localhost";
     $user = "root";
-    $pass = "";
+    $pass = "Nitesh#@?789";
     $dbname = "niteshdb";
 
     $conn = new mysqli($host, $user, $pass, $dbname);
